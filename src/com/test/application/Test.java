156ea2cdc6e95a00aeadb5f056c3e14043ed4885
Hemasundara.X.Saptagiri <Hemasundara.X.Saptagiri@sonymobile.com>
2017-05-29 17:19:20 +0900

    Initial commit

---
+++ src/com/test/application/Test.java
@@ -0,0 +1,11 @@
+package com.test.application;
+
+/**
+ * Created by xp021448 on 5/25/17.
+ */
+public class Test {
+    public  static void main(String args[])
+    {
+        System.out.print("test");
+    }
+}
